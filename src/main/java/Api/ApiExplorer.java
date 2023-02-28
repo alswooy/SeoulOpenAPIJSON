b035db8e895e7afa1656ebb0cf5538886341d6bc
alswooy <jjang990312@naver.com>
2023-02-28 16:09:49 +0900

    no message

--- src/main/java/Api/ApiExplorer.java
+++ src/main/java/Api/ApiExplorer.java
@@ -14,21 +14,21 @@ import com.google.gson.JsonObject;
 
 public class ApiExplorer {
 
-	public static final int COUNT_LIMIT = 1000;
-	public static final String SAMPLE_URL = "http://openapi.seoul.go.kr:8088/sample/json/TbPublicWifiInfo/1/1";
+	public static final int COUNT= 1000;
+	public static final String URL = "http://openapi.seoul.go.kr:8088/sample/json/TbPublicWifiInfo/1/1";
 	
 	public static void loadWifi() throws IOException{
 		
 		int listTotalCount = getListTotalCount();
-		int loopCount = listTotalCount / COUNT_LIMIT + 1;		
+		int loopCount = listTotalCount / COUNT + 1;		
 		
-		for (int i = 0, j = 1; i < loopCount; i++, j += COUNT_LIMIT) {
+		for (int i = 0, j = 1; i < loopCount; i++, j += COUNT) {
 			StringBuilder urlBuilder = new StringBuilder("http://openapi.seoul.go.kr:8088"); 
 			urlBuilder.append("/" + URLEncoder.encode("434c545a586a6a613636494f775847","UTF-8")); 
 			urlBuilder.append("/" + URLEncoder.encode("json","UTF-8"));
 			urlBuilder.append("/" + URLEncoder.encode("TbPublicWifiInfo","UTF-8")); 
 			urlBuilder.append("/" + URLEncoder.encode(String.valueOf(j), "UTF-8")); 
-			urlBuilder.append("/" + URLEncoder.encode(String.valueOf(j + COUNT_LIMIT - 1),"UTF-8")); 
+			urlBuilder.append("/" + URLEncoder.encode(String.valueOf(j + COUNT - 1),"UTF-8")); 
 			
 			URL url = new URL(urlBuilder.toString());
 			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
@@ -57,43 +57,41 @@ public class ApiExplorer {
 			JsonArray rows = tbPublicWifiInfo.get("row").getAsJsonArray();
 			
 			for (JsonElement row : rows) {
-				String wifiMgrNo = row.getAsJsonObject().get("X_SWIFI_MGR_NO").getAsString();
-				String wifiWrdofc = row.getAsJsonObject().get("X_SWIFI_WRDOFC").getAsString();
-				String wifiMainNm = row.getAsJsonObject().get("X_SWIFI_MAIN_NM").getAsString(); 
-				String wifiAdres1 = row.getAsJsonObject().get("X_SWIFI_ADRES1").getAsString();
-				String wifiAdres2 = row.getAsJsonObject().get("X_SWIFI_ADRES2").getAsString();
-				String wifiInstlFloor = row.getAsJsonObject().get("X_SWIFI_INSTL_FLOOR").getAsString();
-				String wifiInstlTy = row.getAsJsonObject().get("X_SWIFI_INSTL_TY").getAsString();
-				String wifiInstlMby = row.getAsJsonObject().get("X_SWIFI_INSTL_MBY").getAsString();
-				String wifiSvcSe = row.getAsJsonObject().get("X_SWIFI_SVC_SE").getAsString();
-				String wifiCmcwr = row.getAsJsonObject().get("X_SWIFI_CMCWR").getAsString();
-				String wifiCnstcYear = row.getAsJsonObject().get("X_SWIFI_CNSTC_YEAR").getAsString();
-				String wifiInoutDoor = row.getAsJsonObject().get("X_SWIFI_INOUT_DOOR").getAsString();
-				String wifiRemars3 = row.getAsJsonObject().get("X_SWIFI_REMARS3").getAsString();
-				Double lat = row.getAsJsonObject().get("LAT").getAsDouble();
-				Double lnt = row.getAsJsonObject().get("LNT").getAsDouble();
-				String workDttm = row.getAsJsonObject().get("WORK_DTTM").getAsString();
-				
-				WifiDto wifiInfo = new WifiDto();
-				
-				wifiInfo.setMGR_NO(wifiMgrNo);
-				wifiInfo.setWRDOFC(wifiWrdofc);
-				wifiInfo.setMAIN_NM(wifiMainNm);
-				wifiInfo.setADRES1(wifiAdres1);
-				wifiInfo.setADRES2(wifiAdres2);
-				wifiInfo.setINSTL_FLOOR(wifiInstlFloor);
-				wifiInfo.setINSTL_TY(wifiInstlTy);
-				wifiInfo.setINSTL_MBY(wifiInstlMby);
-				wifiInfo.setSVC_SE(wifiSvcSe);
-				wifiInfo.setCMCWR(wifiCmcwr);
-				wifiInfo.setCNSTC_YEAR(wifiCnstcYear);
-				wifiInfo.setINOUT_DOOR(wifiInoutDoor);
-				wifiInfo.setREMARS3(wifiRemars3);
-				wifiInfo.setLAT(lat);
-				wifiInfo.setLNT(lnt);
-				wifiInfo.setWORK_DTTM(workDttm);
-				
-				ApiDB.apiInsert(wifiInfo);
+				WifiDto dto = new WifiDto();
+				String MGR_NO = row.getAsJsonObject().get("X_SWIFI_MGR_NO").getAsString();
+				dto.setMGR_NO(MGR_NO);
+				String WRDOFC = row.getAsJsonObject().get("X_SWIFI_WRDOFC").getAsString();
+				dto.setWRDOFC(WRDOFC);
+				String MAIN_NM = row.getAsJsonObject().get("X_SWIFI_MAIN_NM").getAsString(); 
+				dto.setMAIN_NM(MAIN_NM);
+				String ADRES1 = row.getAsJsonObject().get("X_SWIFI_ADRES1").getAsString();
+				dto.setADRES1(ADRES1);
+				String ADRES2 = row.getAsJsonObject().get("X_SWIFI_ADRES2").getAsString();
+				dto.setADRES2(ADRES2);
+				String INSTL_FLOOR = row.getAsJsonObject().get("X_SWIFI_INSTL_FLOOR").getAsString();
+				dto.setINSTL_FLOOR(INSTL_FLOOR);
+				String INSTL_TY = row.getAsJsonObject().get("X_SWIFI_INSTL_TY").getAsString();
+				dto.setINSTL_TY(INSTL_TY);
+				String INSTL_MBY = row.getAsJsonObject().get("X_SWIFI_INSTL_MBY").getAsString();
+				dto.setINSTL_MBY(INSTL_MBY);
+				String SVC_SE = row.getAsJsonObject().get("X_SWIFI_SVC_SE").getAsString();
+				dto.setSVC_SE(SVC_SE);
+				String CMCWR = row.getAsJsonObject().get("X_SWIFI_CMCWR").getAsString();
+				dto.setCMCWR(CMCWR);
+				String CNSTC_YEAR = row.getAsJsonObject().get("X_SWIFI_CNSTC_YEAR").getAsString();
+				dto.setCNSTC_YEAR(CNSTC_YEAR);
+				String INOUT_DOOR = row.getAsJsonObject().get("X_SWIFI_INOUT_DOOR").getAsString();
+				dto.setINOUT_DOOR(INOUT_DOOR);
+				String REMARS3 = row.getAsJsonObject().get("X_SWIFI_REMARS3").getAsString();
+				dto.setREMARS3(REMARS3);
+				Double LAT = row.getAsJsonObject().get("LAT").getAsDouble();
+				dto.setLAT(LAT);
+				Double LNT = row.getAsJsonObject().get("LNT").getAsDouble();
+				dto.setLNT(LNT);
+				String WORK_DTTM = row.getAsJsonObject().get("WORK_DTTM").getAsString();
+				dto.setWORK_DTTM(WORK_DTTM);
+
+				ApiDB.apiInsert(dto);
 				
 			}
 		}
@@ -101,7 +99,7 @@ public class ApiExplorer {
 	
 	public static int getListTotalCount() throws IOException {
 	    
-		URL url = new URL(SAMPLE_URL);
+		URL url = new URL(URL);
 		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 		conn.setRequestMethod("GET");
 
